e44ba5037d40c734014affea4e414bebf27e7936
My Name <me@gm.com>
2025-01-31 19:24:30 +0100

    6.62.1-assn-get-customer : implement the 2 service to list customer and find them by id

---
+++ src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
@@ -0,0 +1,64 @@
+package guru.springframework.spring6restmvc.services;
+
+import guru.springframework.spring6restmvc.model.Customer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Slf4j
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    private Map<UUID, Customer> customerMap;
+
+    public CustomerServiceImpl() {
+
+        Customer customer1 = Customer.builder()
+                            .id(UUID.randomUUID())
+                            .customerName("Mehdi")
+                            .version(1)
+                            .createdDate(LocalDateTime.now())
+                            .lastModifiededDate(LocalDateTime.now())
+                            .build();
+
+        Customer customer2 = Customer.builder()
+                .id(UUID.randomUUID())
+                .customerName("Estelle")
+                .version(1)
+                .createdDate(LocalDateTime.now())
+                .lastModifiededDate(LocalDateTime.now())
+                .build();
+
+        Customer customer3 = Customer.builder()
+                .id(UUID.randomUUID())
+                .customerName("Rayan")
+                .version(1)
+                .createdDate(LocalDateTime.now())
+                .lastModifiededDate(LocalDateTime.now())
+                .build();
+
+
+
+        customerMap = new HashMap<>();
+        customerMap.put(customer1.getId(), customer1);
+        customerMap.put(customer2.getId(), customer2);
+        customerMap.put(customer3.getId(), customer3);
+    }
+
+    @Override
+    public List<Customer> listCustomers() {
+        log.info("Number of customers in DB: " + customerMap.size());
+        return List.of(customerMap.values().toArray(new Customer[0]));
+    }
+
+    @Override
+    public Customer getCustomerById(UUID uuid) {
+        log.info("Selected customer by UUID: " + uuid);
+        return customerMap.get(uuid);
+    }
+}
