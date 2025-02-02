e57ca0a83d44cb5f4484a720be7013266f5ab192
My Name <me@gm.com>
2025-02-02 18:30:31 +0100

    7.71-mock-mvc-config : remove @SpringBootTest in favor of @WebMvcTest limited to BeerController to use MockMVC and mocking our service injected in BeerController and testing the controller

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -1,21 +1,41 @@
 package guru.springframework.spring6restmvc.controller;
 
+import guru.springframework.spring6restmvc.services.BeerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@WebMvcTest(BeerController.class)
 class BeerControllerTest {
 
+
     @Autowired
-    BeerController beerController;
+    private MockMvc mockMvc;
+
+//    @MockBean => deprecated
+    @MockitoBean
+    BeerService beerService;
 
     @Test
-    void getBeerById() {
-        System.out.println(beerController.getBeerById(UUID.randomUUID()));
+    void getBeerById() throws Exception {
+
+        ResultMatcher resultMatcher = m -> m.getResponse().getContentType()
+                                      .equals(MediaType.APPLICATION_JSON);
+
+
+        mockMvc.perform(get("/api/v1/beer/"+UUID.randomUUID()))
+                //.andExpect(resultMatcher.match())
+                .andExpect(status().isOk());
     }
 }
