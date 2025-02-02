edac116d215b0968e90fa0e5e6f9fed8521590fa
My Name <me@gm.com>
2025-02-02 16:31:39 +0100

    6.67-http-delete : create a DeleteMapping controller in charge of deleting a a Beer with a 204 status code returned

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -24,6 +25,13 @@ import java.util.UUID;
 @RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
+
+    @DeleteMapping("{beerId}")
+    public ResponseEntity deleteById(@PathVariable UUID beerId) {
+        beerService.deleteBeerById(beerId);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
 
     @PutMapping(path = {"{beerId}"})
     public ResponseEntity updateById(@PathVariable UUID beerId, @RequestBody Beer beer){
