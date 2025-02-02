34620e78c89b22bc64f88fc5419bc398eb7cbf91
My Name <me@gm.com>
2025-02-02 16:04:21 +0100

    6.66-http-put : define a service updating a Beer + implement the service implementing a beer

--- src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -63,6 +63,18 @@ public class BeerServiceImpl implements BeerService {
     }
 
     @Override
+    public void updateBeerById(UUID beerId, Beer beer) {
+        Beer existing = beerMap.get(beerId);
+        existing.setBeerName(beer.getBeerName());
+        existing.setPrice(beer.getPrice());
+        existing.setUpc(beer.getUpc());
+        existing.setQuantityOnHand(beer.getQuantityOnHand());
+        existing.setUpdateDate(LocalDateTime.now());
+
+        beerMap.put(existing.getId(), existing);
+    }
+
+    @Override
     public List<Beer> listBeers() {
         return List.of(beerMap.values().toArray(new Beer[0]));
     }
