538cc7672d5bf7163a5b3b9f2db2e8d60494fc2f
My Name <me@gm.com>
2025-02-03 11:44:40 +0100

    7.72-mock-mvc-using-mockito : specify the current content-type ofthe reponse sent by the server to the client => UT fails due to the fact taht no response is sent from server

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -4,16 +4,14 @@ import guru.springframework.spring6restmvc.services.BeerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BeerController.class)
@@ -30,14 +28,11 @@ class BeerControllerTest {
     @Test
     void getBeerById() throws Exception {
 
-        ResultMatcher resultMatcher = m -> m.getResponse().getContentType()
-                                      .equals(MediaType.APPLICATION_JSON);
-
-
         mockMvc.perform(
                     get("/api/v1/beer/"+UUID.randomUUID())
                         .accept(MediaType.APPLICATION_JSON)
                 )
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 }
