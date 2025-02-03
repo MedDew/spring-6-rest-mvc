9fd45acfa343b2d277aef16f297dd03b673ebb57
My Name <me@gm.com>
2025-02-03 16:36:37 +0100

    7.76.1-mock-mvc-create-customer : use beforeeach annotation to create a new CustomerServiceImpl object before each UT method + create a UT testing handlePost controller : create a customer payload stub + mock the handlePost controller call + assert the http status code and the presence of the Location header

--- src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerTest.java
@@ -1,8 +1,10 @@
 package guru.springframework.spring6restmvc.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.spring6restmvc.model.Customer;
 import guru.springframework.spring6restmvc.services.CustomerService;
 import guru.springframework.spring6restmvc.services.CustomerServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -10,9 +12,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.hamcrest.core.Is.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,10 +28,38 @@ class CustomerControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockitoBean
     private CustomerService customerService;
 
-    private CustomerServiceImpl customerServiceImpl = new CustomerServiceImpl();
+    private CustomerServiceImpl customerServiceImpl;
+
+    @BeforeEach
+    public void setUp() {
+        customerServiceImpl = new CustomerServiceImpl();
+    }
+
+    @Test
+    public void handlePostTest() throws Exception {
+
+        Customer customerPayload = customerServiceImpl.listCustomers().get(0);
+        customerPayload.setId(null);
+
+        given(customerService.saveNewCustomer(any(Customer.class)))
+        .willReturn(customerServiceImpl.listCustomers().get(1));
+
+        mockMvc.perform(
+                post("/api/v1/customer")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(customerPayload))
+        )
+        .andExpect(status().isCreated())
+        .andExpect(header().exists("Location"));
+
+    }
 
 
     @Test
