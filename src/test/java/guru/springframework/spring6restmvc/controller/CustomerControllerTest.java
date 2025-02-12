b8eeccd41facd343fdb7a95f2c49e1d9a7ecce5b
My Name <me@gm.com>
2025-02-12 12:56:38 +0100

    8.89-exception-optional : refactor the UT's to stick with the CustomerService refactoring

--- src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerTest.java
@@ -17,6 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -151,7 +152,7 @@ class CustomerControllerTest {
 
     @Test
     public void getCustomerByIdNotFound() throws Exception {
-        given(customerService.getCustomerById(any(UUID.class))).willThrow(NotFoundException.class);
+        given(customerService.getCustomerById(any(UUID.class))).willReturn(Optional.empty());
 
         mockMvc.perform(
             get(CustomerController.CUSTOMER_PATH_ID, UUID.randomUUID())
@@ -163,7 +164,7 @@ class CustomerControllerTest {
     public void getCustomerByIdTest() throws Exception {
 
         Customer testCustomer = customerServiceImpl.listCustomers().get(0);
-        given(customerService.getCustomerById(testCustomer.getId())).willReturn(testCustomer);
+        given(customerService.getCustomerById(testCustomer.getId())).willReturn(Optional.of(testCustomer));
 
         mockMvc.perform(
                 get(CustomerController.CUSTOMER_PATH_ID, testCustomer.getId())
