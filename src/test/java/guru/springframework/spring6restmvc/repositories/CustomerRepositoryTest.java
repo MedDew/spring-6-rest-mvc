0e62c71a499dbcd2b855d48ad3afc8d7da63741d
My Name <me@gm.com>
2025-02-13 11:51:52 +0100

    9.97-JPA-test-splice : create UT testing the Customer and Beer entities recording in H2 DB respective tables

---
+++ src/test/java/guru/springframework/spring6restmvc/repositories/CustomerRepositoryTest.java
@@ -0,0 +1,24 @@
+package guru.springframework.spring6restmvc.repositories;
+
+import guru.springframework.spring6restmvc.entities.Customer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class CustomerRepositoryTest {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Test
+    public void testSaveCustomer() {
+        Customer customerSaved = customerRepository.save(Customer.builder()
+                .customerName("New Customer")
+                .build());
+
+        assertThat(customerSaved).isNotNull();
+    }
+}
