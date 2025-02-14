c8a78009df150154d11eedc9db5a8503fc3f0444
My Name <me@gm.com>
2025-02-14 20:09:12 +0100

    refactor deleteById controller to handle the deletion attempt on a not found Beer entity

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -40,7 +40,10 @@ public class BeerController {
 
     @DeleteMapping(BEER_PATH_ID)
     public ResponseEntity deleteById(@PathVariable UUID beerId) {
-        beerService.deleteBeerById(beerId);
+
+        if (!beerService.deleteBeerById(beerId)) {
+            throw new NotFoundException();
+        }
 
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
