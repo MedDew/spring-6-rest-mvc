6cbbea01facd5dd9d25d9dc808d6e5c17fdae540
My Name <me@gm.com>
2025-02-14 20:20:39 +0100

    9.108-jpa-delete-by-id-no-found : fix the mocked UT testing updateById controller

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -103,6 +103,8 @@ class BeerControllerTest {
     public void testUpdateBeer() throws Exception {
         BeerDTO beer =  beerServiceImpl.listBeers().get(0);
 
+        given(beerService.updateBeerById(any(UUID.class), any(BeerDTO.class))).willReturn(Optional.of(beer));
+
         mockMvc.perform(
                 put(BeerController.BEER_PATH_ID, beer.getId())
                 .accept(MediaType.APPLICATION_JSON)
