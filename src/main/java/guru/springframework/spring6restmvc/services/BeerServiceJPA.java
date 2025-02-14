90272a6ef19f30fe7845aa4654dd4fd86f6af2d0
My Name <me@gm.com>
2025-02-14 16:37:12 +0100

    9.104-jpa-save-new-beer : implement the recording of a new beer through handlePost controller + create a UT testing the relevant status code return the 'Location' header and the creation of the new Beer entity

--- src/main/java/guru/springframework/spring6restmvc/services/BeerServiceJPA.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerServiceJPA.java
@@ -41,7 +41,11 @@ public class BeerServiceJPA implements BeerService {
 
     @Override
     public BeerDTO saveNewBeer(BeerDTO beer) {
-        return null;
+
+        return beerMapper.beerToBeerDTO(
+                beerRepository.save(
+                        beerMapper.beerDTOToBeer(beer)
+                ));
     }
 
     @Override
