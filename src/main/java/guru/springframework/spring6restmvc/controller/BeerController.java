7206ae49db0bea45c129f56b562eef18a95a0469
My Name <me@gm.com>
2025-02-14 19:14:01 +0100

    9.106-update-beer-not-found : refactor the code to handle the case when the Beer to update is not found

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -48,7 +48,9 @@ public class BeerController {
     @PutMapping(path = {BEER_PATH_ID})
     public ResponseEntity updateById(@PathVariable UUID beerId, @RequestBody BeerDTO beer){
 
-        beerService.updateBeerById(beerId, beer);
+        if(beerService.updateBeerById(beerId, beer).isEmpty()) {
+            throw new NotFoundException();
+        }
 
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
